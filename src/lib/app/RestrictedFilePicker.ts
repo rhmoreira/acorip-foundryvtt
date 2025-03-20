0f1d79036eb27597ebb1c450dfa3612a0d4dfe07
Renato Hilário Moreira <renato@email.com>
2025-03-20 20:46:00 -0300

    Settings / Player Token Settings / Restricted file picker

--- src/lib/app/RestrictedFilePicker.ts
+++ src/lib/app/RestrictedFilePicker.ts
@@ -1,13 +1,17 @@
+import AcoripLog from "../AcoripLog";
+
 export default class RestrictedFilePicker extends FilePicker  {
  
     private ready: boolean = false;
-    
+    private static INSTANCE: RestrictedFilePicker = null;
+
     constructor(
         private restrictedFolder: string,
         private restrictedTab: "data" | "public" = "data",
         private fileType: FilePicker.Type = "image"
     ){
         super();
+        RestrictedFilePicker.INSTANCE = this;
     }
     
     protected override _render(force?: boolean, options?: Application.RenderOptions<FilePickerOptions>): Promise<void> {
@@ -19,6 +23,7 @@ export default class RestrictedFilePicker extends FilePicker  {
     }
     
     private navigateDefault(): Promise<FilePicker.BrowseResult> {
+        this.fileType;
         super.activateTab(this.restrictedTab);
         return super.browse(this.restrictedFolder);
     }
@@ -31,9 +36,28 @@ export default class RestrictedFilePicker extends FilePicker  {
     }
 
     private browseRestricted(target?: string, options?: FilePicker.BrowseOptions): Promise<FilePicker.BrowseResult> {
-        return target != "assets"
-            ? this.navigateDefault()
-            : super.browse(target, options);
+        return this.restrictedFolder.startsWith(target ?? "")
+            ? super.browse(target, options)
+            : Promise.reject(Error(`Access restricted to unauthorized path >> ${target}`));
     }
+
+    protected override _onChangeTab(_event: MouseEvent | null, _tabs: Tabs, active: this["activeSource"]): void {
+        if (this.ready)
+            if (active != this.restrictedTab)
+                super.activateTab(this.restrictedTab);
+    }
+
+    override close(options?: Application.CloseOptions): Promise<void> {
+        return super.close(options)
+                .then(() => {delete RestrictedFilePicker.INSTANCE});
+    }
+
+    static override createDirectory(source: FilePicker.SourceType, target: string, options?: FilePicker.CreateDirectoryOptions): Promise<string> {
+        if (source === RestrictedFilePicker.INSTANCE.restrictedTab && RestrictedFilePicker.INSTANCE.restrictedFolder.startsWith(target))
+            super.createDirectory(source, target, options);
+        else
+            Promise.reject(Error("Directory creation unauthorized"))
+    }
+
 
 }
--- src/lib/settings/app/PlayerToggleTokenImageSettingMenu.ts
+++ src/lib/settings/app/PlayerToggleTokenImageSettingMenu.ts
@@ -1,8 +1,11 @@
+import RestrictedFilePicker from "../../app/RestrictedFilePicker";
 import { ToggleTokenImageSettingsData } from "../../types/acoriPTypes";
 import utils from "../../utils";
 import ToggleTokenImageSettingsMenu from "./ToggleTokenImageSettingMenu";
 
 export default class PlayerToggleTokenImageSettingMenu extends ToggleTokenImageSettingsMenu{
+
+    private playerTokenSettings: ToggleTokenImageSettingsData;
 
     constructor(){
         super("playerToggleTokenImageSettings");
@@ -10,11 +13,29 @@ export default class PlayerToggleTokenImageSettingMenu extends ToggleTokenImageS
 
     override async activateListeners(_: JQuery) {} //No event handlers necessary at the superclass level
 
-    override getData(_?: Partial<FormApplicationOptions>): Promise<any> {
+    override getData(_?: Partial<FormApplicationOptions>): Promise<ToggleTokenImageSettingsData> {
+        this.playerTokenSettings = this.tokenSettingHelper.playerToggleTokenImageSetting as ToggleTokenImageSettingsData;
         return Promise.resolve({
-            ...this.tokenSettingHelper.playerToggleTokenImageSetting as ToggleTokenImageSettingsData,
+            ...this.playerTokenSettings,
             isPlayer: true
         });
+    }
+
+    protected override _activateCoreListeners(html: JQuery): void {
+        this.attachUploadButtonEvent(html);
+    }
+
+    private attachUploadButtonEvent(html: JQuery): void {
+        html.find("button.rhm.upload-token-image")
+            .on("click", this.triggerRestrictedFilePicker.bind(this));
+    }
+
+    private triggerRestrictedFilePicker(): void{
+        new RestrictedFilePicker(
+            this.playerTokenSettings.defaultTokenImagePath,
+            "data",
+            "image"
+        ).render(true);
     }
 
     override async _updateObject(_: any, formData?: any) {
